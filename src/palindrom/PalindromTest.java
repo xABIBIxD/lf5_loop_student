45a8fd1d7bd36c7217faa524790b47475fda3e65
Bernd Heidemann <bernd.heidemann@googlemail.com>
2020-09-29 11:37:31 +0200

    Palindrom fix 2

--- src/palindrom/PalindromTest.java
+++ src/palindrom/PalindromTest.java
@@ -11,32 +11,32 @@ public class PalindromTest {
     private Palindrom myPalindrom;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         this.myPalindrom = new Palindrom();
     }
 
     @ParameterizedTest(name = "text={0}")
     @CsvSource(value = {"retter", "reliefpfeiler", "anna", "123321"})
-    public void GivenPalindromInOneWord_WhenisPalindrom_ThenReturnTrue(String text){
+    public void GivenPalindromInOneWord_WhenisPalindrom_ThenReturnTrue(String text) {
         assertTrue(myPalindrom.isPalindrom(text));
     }
 
     @ParameterizedTest(name = "text={0}")
     @CsvSource(value = {"Retter", "reliefpfeileR", "aNNa", "123321"})
-    public void GivenPalindromInOneWordButOneSignInUpperCase_WhenisPalindrom_ThenReturnTrue(String text){
+    public void GivenPalindromInOneWordButOneSignInUpperCase_WhenisPalindrom_ThenReturnTrue(String text) {
         assertTrue(myPalindrom.isPalindrom(text));
     }
 
     @ParameterizedTest(name = "text={0}")
     @CsvSource(value = {"sören", "regen", "palindrom", "132321", "Es ist schlechtes Wetter"})
-    public void GivenNoPalindromInOneWord_WhenIsPalindrom_ThenReturnFalse(String text){
+    public void GivenNoPalindromInOneWord_WhenIsPalindrom_ThenReturnFalse(String text) {
         assertFalse(myPalindrom.isPalindrom(text));
     }
 
     @ParameterizedTest(name = "text={0}")
-    @CsvSource(value = {"Anna hetzte Hanna", " O Genie der Herr ehre dein Ego", "Ein Neger mit Gazelle zagt im Regen nie",
+    @CsvSource(value = {"Anna hetzte Hanna", " O Genie der Herr ehre dein Ego", "Die Liebe ist Sieger stets rege ist sie bei Leid",
             "Able was I ere I saw Elba"})
-    public void GivenPalindromInWithWhitespaces_WhenIsPalindrom_ThenReturnTrue(String text){
+    public void GivenPalindromInWithWhitespaces_WhenIsPalindrom_ThenReturnTrue(String text) {
         assertTrue(myPalindrom.isPalindrom(text));
     }
 
