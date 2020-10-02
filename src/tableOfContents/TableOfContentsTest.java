b2cd88a9e09d90fb89d3d9e2a4a94b377b9895f0
Bernd Heidemann <bernd.heidemann@googlemail.com>
2020-10-02 08:53:17 +0200

    added Test

--- src/tableOfContents/TableOfContentsTest.java
+++ src/tableOfContents/TableOfContentsTest.java
@@ -12,15 +12,19 @@ public class TableOfContentsTest {
     private TableOfContents toc;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         this.toc = new TableOfContents();
     }
 
     @DisplayName("Testet alle Fälle, in denen Überschrift und Seitenzahl in eine Zeile passen")
-    @ParameterizedTest (name = "heading={0}, page ={1}, result={2}")
-    @CsvSource(value={"turtle, 34, turtle..........................................34",
+    @ParameterizedTest(name = "heading={0}, page ={1}, result={2}")
+    @CsvSource(value = {"turtle, 34, turtle..........................................34",
             "2.1 Alphanumerische Zeichen, 110, 2.1 Alphanumerische Zeichen....................110"})
-    public void givenCompatibleHeading_WhenCreateEntry_ThenReturnEntryString(String heading, String page, String result){
+    public void givenCompatibleHeading_WhenCreateEntry_ThenReturnEntryString(String heading, String page, String result) {
         assertEquals(result, toc.createEntry(heading, page));
+    }
+
+    public void givenNotCompatibleHeading_WhenCreateEntry_ThenReturnEmptyString() {
+        assertEquals("", toc.createEntry("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod  tempor invidunt ut labore", "34"));
     }
 }
