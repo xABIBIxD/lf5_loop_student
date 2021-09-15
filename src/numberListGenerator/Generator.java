bb49895a4299394952817ca488ce1f29099f8abb
xABIBIx <abdelouadoud1@schule.bremen.de>
2021-09-15 13:56:25 +0200

    15.09.21

--- src/numberListGenerator/Generator.java
+++ src/numberListGenerator/Generator.java
@@ -3,7 +3,7 @@ package numberListGenerator;
 public class Generator {
 
     public String generateNumbers(int start, int end) {
-        return "";
+
     }
 
     public String generateNumbers(int start, int end, int steps, char delimiter) {
--- src/tableOfContents/TableOfContents.java
+++ src/tableOfContents/TableOfContents.java
@@ -2,6 +2,11 @@ package tableOfContents;
 
 public class TableOfContents {
     public String createEntry(String heading, String page) {
-        return "";
+       String apfel = ".";
+       int Kevin = 50-heading.length()-page.length();
+       for (int i = 0; i<Kevin;i++){
+           heading+=apfel;
+       }
+       return heading+page;
     }
 }
